95eeced2b5aa58f68d93a311aa74fc71a0ae5fdb
firemx <higs.ke@gmail.com>
2025-05-10 20:33:39 +0300

    updates

--- frontend/src/pages/AdminDashboard.tsx
+++ frontend/src/pages/AdminDashboard.tsx
@@ -123,7 +123,7 @@ const AdminDashboard = () => {
     });
 
     socket.on('eventUpdated', (updatedEvent) => {
-      setEvents(events.map((e) => (e.id === updatedEvent.id ? updatedEvent : e));
+      setEvents(events.map((e) => (e.id === updatedEvent.id ? updatedEvent : e)));
     });
 
     socket.on('eventDeleted', ({ id }) => {
